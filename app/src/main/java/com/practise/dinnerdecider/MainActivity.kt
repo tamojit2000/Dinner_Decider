206c8924ba36fda489203c0957628e5efcbfd8f0
tamojit2000 <tamojitdas2000@gmail.com>
2020-05-31 01:16:15 +0530

    Bug Fixed

--- app/src/main/java/com/practise/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/practise/dinnerdecider/MainActivity.kt
@@ -2,19 +2,28 @@ package com.practise.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.IntRange
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var Dishes = arrayListOf("Biriyani","Mughlai Paratha","Mixed Fried Rice")
+    var Dishes = arrayListOf("Biriyani","Mughlai Paratha","Mixed Fried Rice","Mixed Chowmein")
+    var initial_index=0
+    var index=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Decide_btn.setOnClickListener(){
             var n=Dishes.size-1
-            var index=(0..n).shuffled().first()
+            while (true){
+                initial_index=index
+                index=(0..n).shuffled().first()
+                if(index!=initial_index){
+                    break
+                }
+                println("Visited")
+            }
             var food=Dishes[index]
             Display.text=food
 
